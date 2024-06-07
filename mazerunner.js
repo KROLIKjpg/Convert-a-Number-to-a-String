f312d4f4347a2f00cb6febc35bd688e1658be30c
KROLIKjpg <114894776+KROLIKjpg@users.noreply.github.com>
2024-06-07 23:44:06 +0700

    Update mazerunner.js

--- mazerunner.js
+++ mazerunner.js
@@ -1,12 +1,12 @@
 function mazeRunner(maze, directions) {
-  // Find the starting position
+
   let [x, y] = findStart(maze);
 
-  // Loop through the directions
+ 
   for (let i = 0; i < directions.length; i++) {
     const dir = directions[i];
 
-    // Move in the given direction
+    
     switch (dir) {
       case 'N':
         y--;
@@ -22,7 +22,7 @@ function mazeRunner(maze, directions) {
         break;
     }
 
-    // Check if the new position is within the maze and is not a wall
+    
     if (
       x < 0 || x >= maze[0].length ||
       y < 0 || y >= maze.length ||
@@ -31,13 +31,13 @@ function mazeRunner(maze, directions) {
       return 'Dead';
     }
 
-    // Check if we've reached the finish point
+    
     if (maze[y][x] === 3) {
       return 'Finish';
     }
   }
 
-  // If we've used all the moves and haven't reached the finish, return 'Lost'
+  
   return 'Lost';
 }
 
